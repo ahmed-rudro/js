911d6599d762d96e548cf2526defe00339814667
ahmed-rudro <ahemdbinsharif2003@gmail.com>
2024-01-23 22:32:28 +0600

    first commit

--- 21-2-scarch.js
+++ 21-2-scarch.js
@@ -1,2 +1,8 @@
+
 const lyrics = 'ahmed bin sharif sun ami valo shala kintu onake basi olos';
-const doesExist = lyrics.includes('ahme')
+const doesExist = lyrics.includes('ahmed');
+console.log(doesExist);
+
+
+
+
