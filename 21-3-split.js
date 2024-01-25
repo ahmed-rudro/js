081ab24724b11d8f441f1c6c21de555bf4fb1bc2
ahmed-rudro <ahemdbinsharif2003@gmail.com>
2024-01-25 17:34:03 +0600

    ahmed

--- 21-3-split.js
+++ 21-3-split.js
@@ -2,4 +2,4 @@ const lyrics = 'ahmed bin sharif sun good boy but not best boy';
 const best = lyrics.slice(5, 11);
 console.log(best);
 
-  
+   
---
+++ 21-4-math.js
@@ -0,0 +1,35 @@
+// const result = Math.pow( 3, 9);
+// console.log(result);
+
+// const num1 = 25;
+// const num2 = 55;
+
+// const gap = Math.abs (num1 - num2);
+// console.log(gap);
+
+// if(gap < 5){
+//     console.log('you guys can be friend');
+
+// }
+// else{
+//     console.log('you guys stay apart');
+// }
+
+
+const number = 2.34453454;
+const fullNumber = Math.round(number);
+console.log(fullNumber)
+
+const number1 = 2.34453454;
+const fullNumber1 = Math.ceil(number1);
+console.log(fullNumber1)
+
+const number2 = 2.34453454;
+const fullNumber2 = Math.floor(number2);
+console.log(fullNumber2)
+
+
+for(let i = 0; i < 20; i++){
+    const rendom = Math.round(Math.random()*6);
+    console.log(rendom);
+}
