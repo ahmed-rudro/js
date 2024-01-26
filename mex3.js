ade4556ec1bf845dad384e38601a99090d93e3b1
ahmed-rudro <ahemdbinsharif2003@gmail.com>
2024-01-26 15:48:16 +0600

    ahmed bin sharif er folder

---
+++ mex3.js
@@ -0,0 +1,13 @@
+const zim = 34;
+const mim = 65;
+const rodro = 54;
+ 
+if(zim > mim && zim > rodro ){
+    console.log('zim');
+}
+else if(mim > zim && mim > rodro){
+    console.log("mim");
+}
+else{
+    console.log("rodro");
+}
